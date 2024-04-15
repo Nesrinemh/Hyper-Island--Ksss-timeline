f7abfd8bf308e05177094271cc3d21c1a3c15c9a
Nesrinemh <nesrine.hadboun@live.fr>
2024-04-15 22:16:06 +0200

    Update dependencies and fix header section formatting

--- src/components/header/HeaderSection.jsx
+++ src/components/header/HeaderSection.jsx
@@ -2,12 +2,11 @@
 
 function HeaderSection() {
   return (
-    <div className=' px-10 border-b pb-4 border-b-gray-300 mt-20'>
-      <h1 className='py-4 text-2xl font-semibold'>Ksss History</h1>
-      <p className=' text-sm text-gray-900'>
-        KSSS is Sweden's largest and the world's second largest
-        sailing club.with over 5,000 members, including over 2,000
-        juniors.
+    <div className=" px-10 border-b pb-4 border-b-gray-300 ">
+      <h1 className="py-4 text-2xl font-semibold">Ksss History</h1>
+      <p className=" text-sm text-gray-900">
+        KSSS is Sweden's largest and the world's second largest sailing
+        club.with over 5,000 members, including over 2,000 juniors.
       </p>
     </div>
   );
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,51 +1,281 @@
-/* eslint-disable react/no-unknown-property */
 import logo from '../../assets/ksss-logo.webp';
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { SearchIcon } from '@heroicons/react/solid';
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+/* eslint-disable react/no-unknown-property */
 
 function Navbar() {
   return (
-    <nav className='flex h-20 border border-b-gray-200 fixed w-full z-20 top-0 start-0 p-4 items-center justify-around'>
-      <img src={logo} className=' h-12' />
-      <h2 className=' text-blue-400 font-bold'>History</h2>
-      <a href='https://www.ksss.se/' className=' font-bold flex'>
-        <span className='border-b border-b-gray-950'>KSSS</span>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke-width='1.5'
-          stroke='currentColor'
-          class='w-4 h-6'
-        >
-          <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            d='m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25'
-          />
-        </svg>
-      </a>
+    // <nav className="flex h-20 border border-b-gray-200 fixed w-full z-20 top-0 start-0 p-4 items-center justify-around">
+    //   <img src={logo} className=" h-12" />
+    //   <h2 className=" text-blue-400 font-bold">History</h2>
+    //   <a href="https://www.ksss.se/" className=" font-bold flex">
+    //     <span className="border-b border-b-gray-950">KSSS</span>
+    //     <svg
+    //       xmlns="http://www.w3.org/2000/svg"
+    //       fill="none"
+    //       viewBox="0 0 24 24"
+    //       stroke-width="1.5"
+    //       stroke="currentColor"
+    //       class="w-4 h-6"
+    //     >
+    //       <path
+    //         stroke-linecap="round"
+    //         stroke-linejoin="round"
+    //         d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+    //       />
+    //     </svg>
+    //   </a>
 
-      <div>
-        <label class='flex items-center relative w-max cursor-pointer select-none'>
-<<<<<<< HEAD
-=======
-          <span class='text-lg font-bold mr-3'>Toggle</span>
->>>>>>> db500b0 (Add header and navbar components)
-          <input
-            type='checkbox'
-            class='appearance-none transition-colors cursor-pointer w-14 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 bg-red-500'
-          />
-          <span class='absolute font-medium text-xs uppercase right-1 text-white'>
-            {' '}
-            SV{' '}
-          </span>
-          <span class='absolute font-medium text-xs uppercase right-8 text-white'>
-            {' '}
-            EN{' '}
-          </span>
-          <span class='w-7 h-7 right-7 absolute rounded-full transform transition-transform bg-gray-200' />
-        </label>
-      </div>
-    </nav>
+    //   <div>
+    //     <label class="flex items-center relative w-max cursor-pointer select-none">
+    //       <input
+    //         type="checkbox"
+    //         class="appearance-none transition-colors cursor-pointer w-14 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 bg-red-500"
+    //       />
+    //       <span class="absolute font-medium text-xs uppercase right-1 text-white">
+    //         {' '}
+    //         SV{' '}
+    //       </span>
+    //       <span class="absolute font-medium text-xs uppercase right-8 text-white">
+    //         {' '}
+    //         EN{' '}
+    //       </span>
+    //       <span class="w-7 h-7 right-7 absolute rounded-full transform transition-transform bg-gray-200" />
+    //     </label>
+    //   </div>
+    // </nav>
+    <Disclosure as="nav" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+            <div className="flex justify-between h-16">
+              <div className="flex px-2 lg:px-0">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block lg:hidden h-8 w-auto"
+                    src={logo}
+                    alt="Workflow"
+                  />
+                  <img
+                    className="hidden lg:block h-8 w-auto"
+                    src={logo}
+                    alt="Workflow"
+                  />
+                  <h2 className=" ml-4 text-[#017AAF] font-bold">History</h2>
+
+                  <a
+                    href="https://www.ksss.se/"
+                    className=" font-bold flex ml-4 text-sm p-0"
+                  >
+                    <span className="border-b border-black">KSSS</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-4 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                      />
+                    </svg>
+                  </a>
+                </div>
+
+                <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
+                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  <a
+                    href="#"
+                    className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                  >
+                    LOGIN
+                  </a>
+                  <a
+                    href="#"
+                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                  >
+                    CONTACT
+                  </a>
+                  <a
+                    href="#"
+                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                  >
+                    ABOUT US
+                  </a>
+                </div>
+              </div>
+
+              <div className="flex-1 md:flex items-center justify-center hidden px-2 lg:ml-6 lg:justify-end">
+                <div className="max-w-lg w-full lg:max-w-xs">
+                  <label htmlFor="search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <SearchIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <input
+                      id="search"
+                      name="search"
+                      className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="Search"
+                      type="search"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex items-center lg:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="hidden lg:ml-4 lg:flex lg:items-center">
+                <button
+                  type="button"
+                  className="flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                {/* <Menu as="div" className="ml-4 relative flex-shrink-0">
+                  <div>
+                    <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700'
+                            )}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700'
+                            )}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm text-gray-700'
+                            )}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu> */}
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="lg:hidden">
+            <div className="pt-2 pb-3 space-y-1">
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+              >
+                LOGIN
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+              >
+                CONTACT
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+              >
+                ABOUT
+              </Disclosure.Button>
+
+              <div className="max-w-lg w-full lg:max-w-xs block md:hidden">
+                <label htmlFor="search" className="sr-only">
+                  Search
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <SearchIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <input
+                    id="search"
+                    name="search"
+                    className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    placeholder="Search"
+                    type="search"
+                  />
+                </div>
+              </div>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
 
