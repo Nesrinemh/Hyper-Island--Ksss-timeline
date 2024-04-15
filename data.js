35078704008bb1921268bd878e0696ae7c3ae868
mvacjar <mvacjar@gmail.com>
2024-04-15 16:58:04 +0200

    MainCard first version done

---
+++ data.js
@@ -0,0 +1,37 @@
+export const archive = [
+  {
+    title: 'Event at Götland',
+    sumary:
+      "KSSS is Swedens largest and the world's second largest sailing club. With over 5.000 members, including over 2.000 juniors.",
+    mainImg: './src/assets/example.png',
+    readMore: 'Read more',
+  },
+  {
+    title: 'Event at Götland',
+    sumary:
+      "KSSS is Swedens largest and the world's second largest sailing club. With over 5.000 members, including over 2.000 juniors.",
+    mainImg: './src/assets/example.png',
+    readMore: 'Read more',
+  },
+  {
+    title: 'Event at Götland',
+    sumary:
+      "KSSS is Swedens largest and the world's second largest sailing club. With over 5.000 members, including over 2.000 juniors.",
+    mainImg: './src/assets/example.png',
+    readMore: 'Read more',
+  },
+  {
+    title: 'Event at Götland',
+    sumary:
+      "KSSS is Swedens largest and the world's second largest sailing club. With over 5.000 members, including over 2.000 juniors.",
+    mainImg: './src/assets/example.png',
+    readMore: 'Read more',
+  },
+  {
+    title: 'Event at Götland',
+    sumary:
+      "KSSS is Swedens largest and the world's second largest sailing club. With over 5.000 members, including over 2.000 juniors.",
+    mainImg: './src/assets/example.png',
+    readMore: 'Read more',
+  },
+];
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderSection from './components/header/HeaderSection';
 import Navbar from './components/header/Navbar';
+import Main from './container/Main';
 
 function App() {
   return (
     <>
       <Navbar />
       <HeaderSection />
+      <Main />
     </>
   );
 }
