59855c8d8c634353862dac16e607a59b36de1fad
mvacjar <mvacjar@gmail.com>
2024-04-15 14:18:44 +0200

    data-aos dependency imported

--- src/components/main/MainCard.jsx
+++ src/components/main/MainCard.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MainCard() {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 500,
+      easing: "ease-in-sine",
+    });
+  }, []);
+
   return <div>MainCard</div>;
 }
 
