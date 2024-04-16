621db6cc1139b9fc23e90db61093bff510863ba4
Nesrinemh <nesrine.hadboun@live.fr>
2024-04-16 14:58:02 +0200

    Refactor Navbar component and add ToggleLanguages component

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,56 +1,13 @@
 import logo from '../../assets/ksss-logo.webp';
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Disclosure } from '@headlessui/react';
 import { SearchIcon } from '@heroicons/react/solid';
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import ToggleLanguages from './ToggleLanguages';
 
 /* eslint-disable react/no-unknown-property */
 
 function Navbar() {
   return (
-    // <nav className="flex h-20 border border-b-gray-200 fixed w-full z-20 top-0 start-0 p-4 items-center justify-around">
-    //   <img src={logo} className=" h-12" />
-    //   <h2 className=" text-blue-400 font-bold">History</h2>
-    //   <a href="https://www.ksss.se/" className=" font-bold flex">
-    //     <span className="border-b border-b-gray-950">KSSS</span>
-    //     <svg
-    //       xmlns="http://www.w3.org/2000/svg"
-    //       fill="none"
-    //       viewBox="0 0 24 24"
-    //       stroke-width="1.5"
-    //       stroke="currentColor"
-    //       class="w-4 h-6"
-    //     >
-    //       <path
-    //         stroke-linecap="round"
-    //         stroke-linejoin="round"
-    //         d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-    //       />
-    //     </svg>
-    //   </a>
-
-    //   <div>
-    //     <label class="flex items-center relative w-max cursor-pointer select-none">
-    //       <input
-    //         type="checkbox"
-    //         class="appearance-none transition-colors cursor-pointer w-14 h-7 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-black focus:ring-blue-500 bg-red-500"
-    //       />
-    //       <span class="absolute font-medium text-xs uppercase right-1 text-white">
-    //         {' '}
-    //         SV{' '}
-    //       </span>
-    //       <span class="absolute font-medium text-xs uppercase right-8 text-white">
-    //         {' '}
-    //         EN{' '}
-    //       </span>
-    //       <span class="w-7 h-7 right-7 absolute rounded-full transform transition-transform bg-gray-200" />
-    //     </label>
-    //   </div>
-    // </nav>
     <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
         <>
@@ -72,6 +29,7 @@ function Navbar() {
 
                   <a
                     href="https://www.ksss.se/"
+                    target="_blank"
                     className=" font-bold flex ml-4 text-sm p-0"
                   >
                     <span className="border-b border-black">KSSS</span>
@@ -96,7 +54,7 @@ function Navbar() {
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <a
                     href="#"
-                    className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    className="border-[indigo-500] text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     LOGIN
                   </a>
@@ -112,6 +70,9 @@ function Navbar() {
                   >
                     ABOUT US
                   </a>
+                  <div>
+                    <ToggleLanguages />
+                  </div>
                 </div>
               </div>
 
@@ -149,80 +110,6 @@ function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="hidden lg:ml-4 lg:flex lg:items-center">
-                <button
-                  type="button"
-                  className="flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                {/* <Menu as="div" className="ml-4 relative flex-shrink-0">
-                  <div>
-                    <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu> */}
-              </div>
             </div>
           </div>
 
@@ -250,7 +137,7 @@ function Navbar() {
               >
                 ABOUT
               </Disclosure.Button>
-
+              <ToggleLanguages />
               <div className="max-w-lg w-full lg:max-w-xs block md:hidden">
                 <label htmlFor="search" className="sr-only">
                   Search
