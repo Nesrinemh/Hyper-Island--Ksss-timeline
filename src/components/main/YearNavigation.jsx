5bf85858dfac4e562138aa0e4fbe58743298bc88
Rahmat-Khavari-Hyper-Island <145808562+Rahmat-Khavari-Hyper-Island@users.noreply.github.com>
2024-04-16 13:04:11 +0200

    commit

--- src/components/main/YearNavigation.jsx
+++ src/components/main/YearNavigation.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import './YearNavigation.css';
 
 function YearNavigation() {
-  return <div>YearNavigation</div>;
+  return (
+    <div className="YearNavigationContainer">
+      <div className="YearNavigation">
+        <button>Now</button>
+        <button>2020</button>
+        <button>2010</button>
+        <button>2000</button>
+        <button>1980</button>
+        <button>1970</button>
+        <button>1960</button>
+        <button>1950</button>
+        <button>1940</button>
+        <button>1930</button>
+      </div>
+    </div>
+  );
 }
 
 export default YearNavigation;
