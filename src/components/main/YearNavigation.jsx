d312a75f472c7196302a1dae3a99eeb03a2799e0
Nesrinemh <nesrine.hadboun@live.fr>
2024-04-17 15:48:06 +0200

    Update YearNavigation button styles

--- src/components/main/YearNavigation.jsx
+++ src/components/main/YearNavigation.jsx
@@ -3,34 +3,34 @@ function YearNavigation() {
   return (
     <div className="max-w-full overflow-x-auto bg-white">
       <div className="inline-flex justify-between items-center py-[1em] px-[1.5em]">
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           Now
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           2020
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           2010
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           2000
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           1990
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           1980
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           1970
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           1960
         </button>
-        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-[0.3em] px-[1em] my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           1950
         </button>
-        <button className=" w-20 py-2 px-3 my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-blue-500 hover:text-white">
+        <button className=" w-20 py-2 px-3 my-0 mx-4 border-solid border-2 border-slate-200 rounded-3xl hover:bg-[#348DB3] hover:text-white">
           1940
         </button>
       </div>
