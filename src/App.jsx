67e4f1cefdb117ad97b141e3210021de40b9de48
mvacjar <mvacjar@gmail.com>
2024-04-17 15:31:08 +0200

    Render YearBookPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './container/Main';
+import YearBookPage from './pages/YearBookPage';
 
 function App() {
   return (
     <>
       <Main />
+      <YearBookPage />
     </>
   );
 }
