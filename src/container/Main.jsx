5e946d19eb7fe8dcfa28b15a4b84df8584daa1be
mvacjar <mvacjar@gmail.com>
2024-04-17 15:22:05 +0200

    YearBookCards Added

--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainCard from '../components/main/MainCard';
+import YearBookCard from '../components/main/YearBookCard';
 
 function Main() {
   return (
     <>
       <MainCard />
+      <YearBookCard />
     </>
   );
 }
