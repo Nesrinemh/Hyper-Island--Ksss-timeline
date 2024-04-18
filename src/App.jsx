168f24cb091f1957331cd5921e47bb7ae592ba45
mvacjar <mvacjar@gmail.com>
2024-04-18 12:05:48 +0200

    Add Back Bar, Carousel and the general body of EventCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import HeaderSection from './components/header/HeaderSection';
 import Navbar from './components/header/Navbar';
+import EventCard from './components/main/EventCard';
 import Main from './container/Main';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <HeaderSection />
+      <EventCard />
       <Main />
     </>
   );
--- src/components/main/EventCard.jsx
+++ src/components/main/EventCard.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import Fredrik from '../../assets/images/fredrik.jpg';
+import Fredrik from '../../assets/fredrik.jpg';
 
 function EventCard() {
   return (
     <>
       <article>
         <section>
-          <h1>Fredrik Lööf wins the bronze medal at the Sydney Olympics</h1>
-          <p>
+          <h1 className="text-md text-bold">
+            Fredrik Lööf wins the bronze medal at the Sydney Olympics
+          </h1>
+          <p className="text-sm">
             In the summer of 2000, Fredrik Lööf secured a bronze medal for
             Sweden in the Finn dinghy class at the Sydney Olympics.{' '}
           </p>
@@ -15,6 +17,57 @@ function EventCard() {
         <figure>
           <img src={Fredrik} alt="Fredrik Lööf" />
         </figure>
+
+        <div className="w-full overflow-x-auto flex flex-row py-10">
+          <div className="snap-center pl-4">
+            <img
+              className="h-full w-full object-cover shrink-0 rounded-lg shadow-xl"
+              src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+            />
+          </div>
+          <div class="snap-center pl-4">
+            <img
+              className="h-full w-full object-cover shrink-0 rounded-lg shadow-xl"
+              src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+            />
+          </div>
+          <div class="snap-center pl-4">
+            <img
+              className="h-full w-full object-cover shrink-0 rounded-lg shadow-xl"
+              src="https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+            />
+          </div>
+          <div class="snap-center pl-4">
+            <img
+              className="h-full w-full object-cover shrink-0 rounded-lg shadow-xl"
+              src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+            />
+          </div>
+          <div class="snap-center pl-4">
+            <img
+              className="h-full w-full object-cover shrink-0 rounded-lg shadow-xl"
+              src="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+            />
+          </div>
+          <div class="snap-center px-4">
+            <img
+              className="h-full w-full object-cover shrink-0 w-80 h-40 rounded-lg shadow-xl"
+              src="https://images.unsplash.com/photo-1559333086-b0a56225a93c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80"
+            />
+          </div>
+        </div>
+        <p className="text-sm px-12 pb-4">
+          Lööf's achievement marked a significant moment in Swedish sailing
+          history, showcasing his skill and determination on the international
+          stage.
+        </p>
+        <p className="text-sm px-12">
+          With his impressive performance, Lööf demonstrated the prowess of
+          Swedish sailors and added to the country's Olympic medal tally. His
+          accomplishment in the Finnjolle event not only brought pride to Sweden
+          but also highlighted the excellence of athletes in the sport of
+          sailing.
+        </p>
       </article>
     </>
   );
