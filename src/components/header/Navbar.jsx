2c38d12f0a99b635537efe6a05bdb6edb647c70a
Nesrinemh <nesrine.hadboun@live.fr>
2024-04-25 10:12:20 +0200

    minor changes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -6,7 +6,7 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline';
 function Navbar() {
   return (
     <>
-      <Disclosure as="nav" className="bg-white    shadow ">
+      <Disclosure as="nav" className="bg-white shadow">
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
@@ -21,7 +21,7 @@ function Navbar() {
                     />
                   </div>
 
-                  <div className="flex justify-center items-center w-[20em] space-x-10 ">
+                  <div className="flex justify-center items-center w-[20em] space-x-10 md:w-[40em] ">
                     {/* Title */}
 
                     <h2 className=" ml-4 text-[#017AAF] font-bold">History</h2>
@@ -54,43 +54,11 @@ function Navbar() {
                   </div>
 
                   {/* Large screen navbar */}
-                  <div className="hidden lg:ml-6 lg:flex lg:space-x-8 ">
-                    {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                    <a
-                      href="#"
-                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-[#017AAF] inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    >
-                      About us
-                    </a>
-                    <a
-                      href="#"
-                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-[#017AAF] inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    >
-                      Log in / Signup
-                    </a>
-                    <a
-                      href="#"
-                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-[#017AAF] inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    >
-                      Be a member
-                    </a>
-                    <a
-                      href="#"
-                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-[#017AAF] inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    >
-                      Year book
-                    </a>
-                    <a
-                      href="#"
-                      className="border-transparent text-gray-500 hover:border-gray-300 hover:text-[#017AAF] inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                    >
-                      Contact us
-                    </a>
-                  </div>
                 </div>
 
                 <div className="flex items-center  lg:hidden">
                   {/* Mobile menu button */}
+
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[#017AAF] ">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
@@ -103,12 +71,12 @@ function Navbar() {
               </div>
             </div>
 
-            <Disclosure.Panel className="lg:hidden w-40 absolute right-0   bg-white   z-50">
-              <div className="pt-2 pb-3 space-y-1">
+            <Disclosure.Panel className="w-40 absolute right-0 bg-white z-50">
+              <div className="pt-2 pb-3 space-y-1 backdrop-blur-lg">
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="border-transparent text-[#017AAF] hover:bg-gray-50 hover:border-gray-300 hover:font-bold block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
+                  className="border-transparent text-black hover:bg-gray-50 block pl-3 pr-4 py-2 border-l-4 text-xs font-normal"
                   s
                 >
                   About us
@@ -116,28 +84,28 @@ function Navbar() {
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="border-transparent text-[#017AAF] hover:bg-gray-50 hover:border-gray-300 hover:font-bold block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
+                  className="border-transparent text-black hover:bg-gray-5 block pl-3 pr-4 py-2 border-l-4 text-xs font-normal"
                 >
-                  Log in / Signup
+                  Log in/Sign up
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="border-transparent text-[#017AAF] hover:bg-gray-50 hover:border-gray-300 hover:font-bold block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
+                  className="border-transparent text-[#017AAF] hover:bg-gray-5 block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
                 >
                   Be a member
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="border-transparent text-[#017AAF] hover:bg-gray-50 hover:border-gray-300 hover:font-bold block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
+                  className="border-transparent text-[#017AAF] hover:bg-gray-5 block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
                 >
                   Year book
                 </Disclosure.Button>
                 <Disclosure.Button
                   as="a"
                   href="#"
-                  className="border-transparent text-[#017AAF] hover:bg-gray-50 hover:border-gray-300 hover:font-bold block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
+                  className="border-transparent text-[#017AAF] hover:bg-gray-5 block pl-3 pr-4 py-2 border-l-4 text-xs font-medium"
                 >
                   Contact us
                 </Disclosure.Button>
